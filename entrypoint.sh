f4eb69592db724ad68392a8bccaee3cd4417197e
Murillo Torres <37597633+murillotorres@users.noreply.github.com>
2025-03-23 23:14:11 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-set -e
-
-# Verifica se o WordPress já está instalado
-if [ ! -f /var/www/html/wp-config.php ]; then
-  echo "Copiando arquivos do WordPress para a pasta backend..."
-  cp -r /usr/src/wordpress/* /var/www/html/
-fi
-
-# Inicia o WordPress normalmente
+echo "Iniciando o WordPress..."
 exec docker-entrypoint.sh apache2-foreground
